0603138193882b56ab99760a75dfe357d4546001
maxniu97 <maxniu1997@gmail.com>
2022-10-04 19:16:10 -0600

    category finished on home, debugging item spacings.

---
+++ Fresh/Models/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fresh.Models
+{
+    public class ProductCategory
+    {
+        public string ImageName  { get; set; }
+        public string Header { get; set; }
+    }
+}
--- Fresh/ViewModels/firstVM.cs
+++ Fresh/ViewModels/firstVM.cs
@@ -8,6 +8,7 @@ namespace Fresh.ViewModels
     public class firstVM
     {
         public List<CarouselItemModel> Carousels { get; set; }
+        public List<ProductCategory> Categories { get; set; }
         public firstVM()
         {
             Carousels = new List<CarouselItemModel>();
@@ -15,6 +16,18 @@ namespace Fresh.ViewModels
             Carousels.Add(new CarouselItemModel { ImageName = "homeBanner2.jpg" });
             Carousels.Add(new CarouselItemModel { ImageName = "homeBanner3.jpg" });
             Carousels.Add(new CarouselItemModel { ImageName = "homeBanner4.jpg" });
+
+            Categories = new List<ProductCategory>();
+            Categories.Add(new ProductCategory { ImageName = "bread.JPG", Header = "Bread" });
+            Categories.Add(new ProductCategory { ImageName = "canned.jpg", Header = "Canned Food" });
+            Categories.Add(new ProductCategory { ImageName = "deli.jpg", Header = "Deli" });
+            Categories.Add(new ProductCategory { ImageName = "drinks.jpg", Header = "Soft Drinks" });
+            Categories.Add(new ProductCategory { ImageName = "fruit.jpg", Header = "Fruits" });
+            Categories.Add(new ProductCategory { ImageName = "meat.jpeg", Header = "Meat" });
+            Categories.Add(new ProductCategory { ImageName = "seafood.jpg", Header = "Seafood" });
+            Categories.Add(new ProductCategory { ImageName = "seasoning.jpg", Header = "Spices" });
+            Categories.Add(new ProductCategory { ImageName = "snacks.jpg", Header = "Snacks" });
+            Categories.Add(new ProductCategory { ImageName = "vegetable.jpg", Header = "Vegetables" });
         }
     }
 }
